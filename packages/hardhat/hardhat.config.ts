0d02f0a216f8fbee781fb099622da40e1277e10f
havidtech <76119744+havidtech@users.noreply.github.com>
2024-09-28 18:27:00 +0100

    fix: add scroll alchemy rpc

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -107,11 +107,11 @@ const config: HardhatUserConfig = {
       accounts: [deployerPrivateKey],
     },
     scrollSepolia: {
-      url: "https://sepolia-rpc.scroll.io",
+      url: `https://scroll-sepolia.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
     },
     scroll: {
-      url: "https://rpc.scroll.io",
+      url: `https://scroll-mainnet.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
     },
     pgn: {
