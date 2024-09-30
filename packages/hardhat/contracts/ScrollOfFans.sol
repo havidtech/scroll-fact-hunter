42654020f1596e3db6cfe41c257473c339676919
havidtech <76119744+havidtech@users.noreply.github.com>
2024-09-30 12:27:05 +0100

    fix: fix bug causing contract  to revert

--- packages/hardhat/contracts/ScrollOfFans.sol
+++ packages/hardhat/contracts/ScrollOfFans.sol
@@ -5,8 +5,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import "hardhat/console.sol";
-
 
 contract ScrollOfFans is ERC721, Ownable {
 
@@ -14,34 +12,25 @@ contract ScrollOfFans is ERC721, Ownable {
 
     function mint(uint256 tokenId, bytes memory signature) external {
         require(_ownerOf(tokenId) == address(0), "Token already minted");
-        // Import statement moved to the top of the file
-        console.log("Minting token with ID:", tokenId);
-        console.logBytes(signature);
 
-
-        console.logBytes(abi.encode(msg.sender, tokenId));
         bytes32 messageHash = keccak256(abi.encode(msg.sender, tokenId));
         bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(messageHash);
 
-        console.logBytes32(messageHash);
-        console.logBytes32(ethSignedMessageHash);
-
         address signer = ECDSA.recover(ethSignedMessageHash, signature);
-        console.log("Recovered signer:", signer);
-        console.log("Contract owner:", owner());
         require(signer == owner(), "Invalid signature");
 
         _mint(msg.sender, tokenId);
     }
 
     // Override _transfer to prevent transfers
-    function _update(address to,uint256 tokenId, address auth ) internal override(ERC721) returns(address) {
-        address from = ownerOf(tokenId);
-        if(from != address(0) && to !=address(0)){
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721) returns(address) {
+        address from = _ownerOf(tokenId);
+
+        if(from != address(0) && to != address(0)) {
             revert("Soulbound: Transfer Failed");
         }
 
-        return super._update(to,tokenId,auth);
+        return super._update(to, tokenId, auth);
     }
 
 }
